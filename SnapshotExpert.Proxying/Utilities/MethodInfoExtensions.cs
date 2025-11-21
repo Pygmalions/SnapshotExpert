5c0de18f6cc75dac47902c392f6043bcf88199d4
Haoyu Jia <57647667+OldVincent@users.noreply.github.com>
2025-11-22 01:10:30 +0900

    This utility has been correctly placed in proxying module.

---
+++ SnapshotExpert.Proxying/Utilities/MethodInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SnapshotExpert.Remoting.Utilities;
+
+public static class MethodInfoExtensions
+{
+    extension(MethodInfo self)
+    {
+        public Type DelegateType
+        {
+            get
+            {
+                var types = self
+                    .GetParameters()
+                    .Select(parameter => parameter.ParameterType)
+                    .Append(self.ReturnType)
+                    .ToArray();
+                return Expression.GetDelegateType(types.ToArray());
+            }
+        }
+    }
+}
