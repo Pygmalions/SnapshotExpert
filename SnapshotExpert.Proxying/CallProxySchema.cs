16815bf2472bf8693b7b1515f591e554db0a0d39
Haoyu Jia <jia_vincent@outlook.com>
2025-11-22 18:22:20 +0900

    Add method to generate schema for method calls.

---
+++ SnapshotExpert.Proxying/CallProxySchema.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using DocumentationParser;
+using SnapshotExpert.Data;
+using SnapshotExpert.Data.Schemas.Primitives;
+
+namespace SnapshotExpert.Remoting;
+
+public static class CallProxySchema
+{
+    /// <summary>
+    /// Create a schema for arguments of a method call for the specified method.
+    /// </summary>
+    /// <param name="method">Method to create method call schema for.</param>
+    /// <param name="serializers">Serializers to use.</param>
+    /// <param name="documentation">Documentation to inject into the schema.</param>
+    /// <returns>Snapshot schema for method calls of the specified method.</returns>
+    public static ObjectSchema For(MethodInfo method, ISerializerProvider serializers,
+        IDocumentationProvider? documentation = null)
+    {
+        OrderedDictionary<string, SnapshotSchema>? requiredProperties = null;
+        OrderedDictionary<string, SnapshotSchema>? optionalProperties = null;
+
+        foreach (var parameter in method.GetParameters())
+        {
+            var serializer = serializers.RequireSerializer(parameter.ParameterType);
+            ref var selectedProperties =
+                ref parameter.HasDefaultValue ? ref optionalProperties : ref requiredProperties;
+            selectedProperties ??= new OrderedDictionary<string, SnapshotSchema>();
+            selectedProperties.Add(parameter.Name ?? parameter.Position.ToString(), serializer.Schema);
+        }
+
+        return new ObjectSchema
+        {
+            Title = method.Name,
+            Description = documentation?.GetEntry(method)?.Summary,
+            RequiredProperties = requiredProperties,
+            OptionalProperties = optionalProperties
+        };
+    }
+}
---
+++ Tests/SnapshotExpert.Proxying.Test/TestCallProxySchema.cs
@@ -0,0 +1,36 @@
+﻿using SnapshotExpert.Data.Schemas.Primitives;
+
+namespace SnapshotExpert.Remoting.Test;
+
+[TestFixture, TestOf(typeof(CallProxySchema))]
+public class TestCallProxySchema
+{
+    private class SampleClass
+    {
+        public int Add(int a, int b, int c = 0) => a + b;
+    }
+
+    [Test]
+    public void GenerateSchema()
+    {
+        var serializer = new SerializerContainer();
+
+        var schema = CallProxySchema.For(typeof(SampleClass).GetMethod(nameof(SampleClass.Add))!, serializer);
+
+        Assert.That(schema, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(schema.RequiredProperties, Is.Not.Null);
+            Assert.That(schema.OptionalProperties, Is.Not.Null);
+        }
+
+        Assert.That(schema.RequiredProperties.Count, Is.EqualTo(2));
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(schema.RequiredProperties["a"], Is.TypeOf<IntegerSchema>());
+            Assert.That(schema.RequiredProperties["b"], Is.TypeOf<IntegerSchema>());
+            Assert.That(schema.RequiredProperties.ContainsKey("c"), Is.False);
+            Assert.That(schema.OptionalProperties["c"], Is.TypeOf<IntegerSchema>());
+        }
+    }
+}
