026739f7d4891d4e4134086b94039f4bc8473d81
Haoyu Jia <jia_vincent@outlook.com>
2025-11-26 17:11:27 +0900

    Add automatic parsing from string values for complex types.

--- SnapshotExpert/Data/ISnapshotConvertible.cs
+++ SnapshotExpert/Data/ISnapshotConvertible.cs
@@ -24,7 +24,7 @@ public static class SnapshotConvertibleExtensions
         /// Check if this node is bound to a null value.
         /// </summary>
         public bool IsNull => self?.Value is NullValue;
-        
+
         public ObjectValue AsObject => self?.Value as ObjectValue ?? throw new InvalidCastException(
             $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to a object value.");
 
@@ -45,22 +45,22 @@ public static class SnapshotConvertibleExtensions
             ? (byte)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'byte'.");
-        
+
         public sbyte AsSByte => self?.Value is IInteger32Value value
             ? (sbyte)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'sbyte'.");
-        
+
         public short AsInt16 => self?.Value is IInteger32Value value
             ? (short)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'short'.");
-        
+
         public ushort AsUInt16 => self?.Value is IInteger32Value value
             ? (ushort)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'ushort'.");
-        
+
         public int AsInt32 => self?.Value is IInteger32Value value
             ? value.Value
             : throw new InvalidCastException(
@@ -70,7 +70,7 @@ public static class SnapshotConvertibleExtensions
             ? (uint)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'uint'.");
-        
+
         public long AsInt64 => self?.Value is IInteger64Value value
             ? value.Value
             : throw new InvalidCastException(
@@ -80,7 +80,7 @@ public static class SnapshotConvertibleExtensions
             ? (ulong)value.Value
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'ulong'.");
-        
+
         public double AsDouble => self?.Value is IFloat64Value value
             ? value.Value
             : throw new InvalidCastException(
@@ -96,19 +96,44 @@ public static class SnapshotConvertibleExtensions
             : throw new InvalidCastException(
                 $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'decimal'.");
 
-        public DateTimeOffset AsDateTimeOffset => self?.Value is DateTimeValue value
-            ? value.Value
-            : throw new InvalidCastException(
-                $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'DateTimeOffset'.");
+        public DateTimeOffset AsDateTimeOffset
+        {
+            get
+            {
+                if (self?.Value is DateTimeValue value)
+                    return value.Value;
+                if (self?.Value is StringValue stringValue &&
+                    DateTimeOffset.TryParse(stringValue.Value, out var parsedValue))
+                    return parsedValue;
+                throw new InvalidCastException(
+                    $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'DateTimeOffset'.");
+            }
+        }
 
-        public byte[] AsBytes => self?.Value is BinaryValue value
-            ? value.Value
-            : throw new InvalidCastException(
-                $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'byte[]'.");
+        public byte[] AsBytes
+        {
+            get
+            {
+                if (self?.Value is BinaryValue value)
+                    return value.Value;
+                if (self?.Value is StringValue stringValue)
+                    return Convert.FromBase64String(stringValue.Value);
+                throw new InvalidCastException(
+                    $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'byte[]'.");
+            }
+        }
 
-        public Guid AsGuid => self?.Value is BinaryValue value
-            ? value.AsGuid
-            : throw new InvalidCastException(
-                $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'Guid'.");
+        public Guid AsGuid
+        {
+            get
+            {
+                if (self?.Value is BinaryValue value)
+                    return value.AsGuid;
+                if (self?.Value is StringValue stringValue)
+                    return Guid.Parse(stringValue.Value);
+                throw new InvalidCastException(
+                    $"Cannot convert snapshot value '{self?.Value?.GetType().ToString() ?? "<Empty>"}' to 'Guid'.");
+            }
+        }
     }
 }
